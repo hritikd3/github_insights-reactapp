5abdfa699d6fb1e58c5456a333c5cab31a79970f
hritikd3 <hritikdangi3@gmail.com>
2023-06-04 10:48:57 +0530

    done with initial routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
-    <div>
+    <Router>
+      <Switch >
+
+    <Route path="/" exact="{true}">
       <Dashboard></Dashboard>
+
+    </Route>
+    <Route path="/login">
       <Login />
+
+    </Route>
+    <Route path="*">
+
       <Error />
-    </div>
+    </Route>
+      </ Switch>
+    </Router>
   );
 }
 
