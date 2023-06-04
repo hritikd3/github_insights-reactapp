7563d876319d9da9286db13557799a0e55464ffc
hritikd3 <hritikdangi3@gmail.com>
2023-06-04 10:49:15 +0530

    developed the 404 page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,7 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 const Error = () => {
-  return <h2>error page</h2>;
+  return (
+    <><Wrapper>
+      <div>
+        <h1>404</h1>
+        <h3>Oops! You weren't supposed to see this </h3>
+
+    <Link to='/' className="btn">
+      Take me to Homem Page
+    </Link>
+      </div>
+    </Wrapper>
+    </>
+  );
 };
 const Wrapper = styled.section`
   min-height: 100vh;
@@ -11,11 +23,12 @@ const Wrapper = styled.section`
   background: var(--clr-primary-10);
   text-align: center;
   h1 {
-    font-size: 10rem;
+    font-size: 12rem;
   }
   h3 {
+  font-size: 2rem;
     color: var(--clr-grey-3);
-    margin-bottom: 1.5rem;
+    margin-bottom: 2.5rem;
   }
 `;
 export default Error;
