cec345e78f33a01926798f9cf75886a370ab677e
hritikd3 <hritikdangi3@gmail.com>
2023-06-25 21:29:57 +0530

    fix : routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import React from "react";
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
   return (
     <Router>
-      <Switch >
-
-    <Route path="/" exact="{true}">
-      <Dashboard></Dashboard>
-
-    </Route>
-    <Route path="/login">
-      <Login />
-
-    </Route>
-    <Route path="*">
-
-      <Error />
-    </Route>
-      </ Switch>
+      <Switch>
+        <Route path="/" exact="{true}">
+          <Dashboard></Dashboard>
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
     </Router>
   );
 }
