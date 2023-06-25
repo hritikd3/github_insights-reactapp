b774a48dca24a8978afeb7a72540306ca4481172
hritikd3 <hritikdangi3@gmail.com>
2023-06-25 21:30:32 +0530

    managed: context

--- src/context/context.js
+++ src/context/context.js
@@ -5,3 +5,18 @@ import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+
+//now we have accces to the provider ,consumer - GithubProvider.Provider
+const GithubContext= React.createContext();
+const GithubProvider=({children})=>{
+return (
+    <GithubContext.Provider value={'testing '}>
+        {children}
+    </GithubContext.Provider>
+);
+};
+
+export { GithubProvider, GithubContext };
+
+
